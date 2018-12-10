8316d88a51ee16dc7d9e03c7c8fae17468cc2f20
Ashley Busch <ashley.busch.2022@student.lhschools.org>
2018-12-10 10:08:24 -0500

    Attempting to fix JavaScript

--- script.js
+++ script.js
@@ -2,21 +2,17 @@
 <html>
 <body>
 
-<p>If you want to see more pictures of the Licking Heights Marching Band, click on the
-  link below. You can see pictures of the band from 2013 to now, and there will be 
-  plenty more to come!</p>
-  
   <button onclick="myFunction()">Click Here</button>
-  
+
   <p id="demo"></p>
-  
+
   <script>
   function myFunction() {
       var str = "LH Marching Band photos";
-      var result = str.link ("https://www.kennytphoto.com/Licking-Heights-High-School/Band");
+      var result = str.link (a href="https://www.kennytphoto.com/Licking-Heights-High-School/Band");
       document.getElementById ("demo").innerHTML = result;
   }
   </script>
-  
+
   </body>
   </html>
