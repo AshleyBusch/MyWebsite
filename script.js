fc9fdbb48475b9f905eb31bd095627b3a1906e68
Ashley Busch <ashley.busch.2022@student.lhschools.org>
2018-12-10 11:42:02 -0500

    Fixed js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-<!DOCTYPE>
-<html>
-<body>
 
-  <button onclick="myFunction()">Click Here</button>
-
-  <p id="demo"></p>
-
-  <script>
   function myFunction() {
       var str = "LH Marching Band photos";
-      var result = str.link (a href="https://www.kennytphoto.com/Licking-Heights-High-School/Band");
+      var result = str.link ("https://www.kennytphoto.com/Licking-Heights-High-School/Band");
       document.getElementById ("demo").innerHTML = result;
   }
-  </script>
-
-  </body>
-  </html>
